0e61d2036173bce87719ae7cd454f990763ae036
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-05-30 14:59:31 -0400

    Made config file for boundaries

--- Assets/_InDevelopment/Chase/Refactored/PartBoundsLocker.cs
+++ Assets/_InDevelopment/Chase/Refactored/PartBoundsLocker.cs
@@ -4,12 +4,9 @@ namespace DeformationSystem
 {
     public class PartBoundsLocker : MonoBehaviour
     {
-        [Tooltip("X, Y, Z bounds. Will prevent the part from moving outside of these bounds. Based in world origin")]
-        [SerializeField] private FloatRange[] _bounds;
+        [Tooltip("The reference to the boundary setup for the bounds locker to use.")]
+        [SerializeField] private BoundsConfig _bounds;
         
-        [Tooltip("If true, the bounds will be based on the starting position of the object.")]
-        [SerializeField] private bool _fromStartingPosition;
-
         private Vector3 _startingPosition;
         
         private void Awake()
@@ -25,7 +22,7 @@ namespace DeformationSystem
         private void ClampPosition()
         {
             var position = transform.position;
-            var offset = _fromStartingPosition ? _startingPosition : Vector3.zero;
+            var offset = _bounds.FromStartingPosition ? _startingPosition : Vector3.zero;
             for (int i = 0; i < 3; i++)
             {
                 position[i] = Mathf.Clamp(position[i], _bounds[i].Min + offset[i], _bounds[i].Max + offset[i]);
