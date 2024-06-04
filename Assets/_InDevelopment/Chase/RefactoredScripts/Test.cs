9656490a7d5967038d435a45527565e58c3fa4a9
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-06-04 09:54:28 -0400

    TestOne

---
+++ Assets/_InDevelopment/Chase/RefactoredScripts/Test.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DeformationSystem
+{
+	public class Test : MonoBehaviour
+	{
+		// Enums
+
+		// static, const, and readonly
+
+		// Events
+
+		// SerializeFields, include tooltips
+
+		// Properties and fields
+
+		// Constructors / Finalizers
+
+		// Unity Methods
+
+		// Other Methods
+
+		// Local Classes
+
+	}
+}
