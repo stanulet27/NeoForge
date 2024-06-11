da8e598b2113037e5392765a69e11841963bbeab
chris <stanulet.1@osu.edu>
2024-06-11 12:01:33 -0400

    Add untracked files from last commit

---
+++ Assets/_InDevelopment/Chris/Development/CameraSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+namespace _InDevelopment.Chris.Development
+{
+    public class CameraSwitcher : MonoBehaviour
+    {
+       [SerializeField] private Animator animator;
+
+        void Start()
+        {
+           
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                animator.Play("Overview Position");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                animator.Play("Heating Position");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                animator.Play("Forging Position");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                animator.Play("Cooling Position");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                animator.Play("Planning Position");
+            }
+        }
+    }
+}
