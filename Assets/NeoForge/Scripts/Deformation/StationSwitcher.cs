9ef4cd9b8239ff4b47d21eb35e5a0089ff829b6e
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-06-17 14:35:53 -0400

    Moved files to appropriate folders

---
+++ Assets/NeoForge/Scripts/Deformation/StationSwitcher.cs
@@ -0,0 +1,25 @@
+using SharedData;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace NeoForge.Deformation
+{
+    public class StationSwitcher : MonoBehaviour
+    {
+        [Tooltip("The event that is invoked when the station is changed. Should be found in Station Controller")]
+        [SerializeField]
+        private UnityEvent<Station> _changeStation;
+
+        [Tooltip("The station that the player will switch to")] [SerializeField]
+        private Station _station;
+
+        /// <summary>
+        /// Changes the station to the one specified in the inspector
+        /// </summary>
+        public void Invoke()
+        {
+            _changeStation.Invoke(_station);
+        }
+    }
+}
